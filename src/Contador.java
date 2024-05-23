e0d1160ba67821fa3acfe82c3fadc33f97351675
Eduardo0404 <88066935+Eduardo0404@users.noreply.github.com>
2024-05-23 00:56:45 -0300

    contador

--- src/Contador.java
+++ src/Contador.java
@@ -25,7 +25,7 @@ public class Contador {
             }
 
         }
-        
+
         static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
             if (parametroUm >= parametroDois) {
                 throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
